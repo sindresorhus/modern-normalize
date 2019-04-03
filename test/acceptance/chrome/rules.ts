2e1a177de07881592860a4037add93ef35e9a8ec
Rafael Ferrari <me@rferrari.net>
2019-04-03 08:25:48 -0300

    Add validation tests to check obsolete styles

--- test/acceptance/chrome/rules.ts
+++ test/acceptance/chrome/rules.ts
@@ -1,7 +1,7 @@
 import { Selector } from 'testcafe';
 
-fixture `Firefox Tests`
-    .page `http://localhost:8080/test/page/index.html`;
+fixture `Chrome Rules Tests`
+    .page `http://localhost:8080/test/page/with-css.html`;
 
 test('Remove the margin in all browsers.', async t => {
     await t
@@ -15,7 +15,7 @@ test('Improve consistency of default fonts in all browsers.', async t => {
     await t
         .expect(Selector('body').getStyleProperty('font-family')).eql('-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"');
 });
-    
+
 test('Add the correct height in Firefox.', async t => {
     await t
         .expect(Selector('hr[data-test--hr]').getStyleProperty('height')).eql('2px');
@@ -23,11 +23,11 @@ test('Add the correct height in Firefox.', async t => {
 
 test('Add the correct text decoration in Chrome, Edge, and Safari.', async t => {
     await t
-        .expect(Selector('abbr[data-test--abbr]').getStyleProperty('text-decoration')).eql('underline dotted');
+        .expect(Selector('abbr[data-test--abbr]').getStyleProperty('text-decoration')).eql('underline dotted rgb(0, 0, 0)');
 });
 
 test('Add the correct font weight in Chrome, Edge, and Safari.', async t => {
     await t
-        .expect(Selector('b[data-test--bold]').getStyleProperty('font-weight')).eql('700')
-        .expect(Selector('strong[data-test--bold]').getStyleProperty('font-weight')).eql('700');
+        .expect(Selector('b[data-test--bold]').getStyleProperty('font-weight')).eql('900')
+        .expect(Selector('strong[data-test--bold]').getStyleProperty('font-weight')).eql('900');
 });
