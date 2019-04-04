b649685c3109cf153a65b135df57b314af424649
Rafael Ferrari <me@rferrari.net>
2019-04-04 01:02:11 -0300

    Update acceptance tests

--- test/acceptance/chrome/rules.ts
+++ test/acceptance/chrome/rules.ts
@@ -80,3 +80,92 @@ test('Prevent `sub` and `sup` elements from affecting the line height in all bro
         .expect(Selector('sub[data-test--subsup]').getStyleProperty('bottom')).eql('-3px')
         .expect(Selector('sup[data-test--subsup]').getStyleProperty('top')).eql('-6px');
 });
+
+test('Change the font styles in all browsers.', async t => {
+    await t
+        .expect(Selector('[data-test--forms-1]').getStyleProperty('font-family')).eql('MYCUSTOMFONT')
+        .expect(Selector('[data-test--forms-1]').getStyleProperty('font-size')).eql('16px')
+        .expect(Selector('[data-test--forms-1]').getStyleProperty('line-height')).eql('18.4px');
+});
+
+test('Remove the margin in Firefox and Safari.', async t => {
+    await t
+        .expect(Selector('[data-test--forms-1]').getStyleProperty('margin-top')).eql('0px')
+        .expect(Selector('[data-test--forms-1]').getStyleProperty('margin-right')).eql('0px')
+        .expect(Selector('[data-test--forms-1]').getStyleProperty('margin-bottom')).eql('0px')
+        .expect(Selector('[data-test--forms-1]').getStyleProperty('margin-left')).eql('0px');
+});
+
+test('Remove the inheritance of text transform in Firefox.', async t => {
+    await t
+        .expect(Selector('button[data-test--forms-1]').getStyleProperty('text-transform')).eql('none')
+        .expect(Selector('select[data-test--forms-1]').getStyleProperty('text-transform')).eql('none');
+});
+
+test('Correct the inability to style clickable types in iOS and Safari.', async t => {
+    await t
+        .expect(Selector('button[data-test--forms-1]').getStyleProperty('-webkit-appearance')).eql('button')
+        .expect(Selector('[type="button"][data-test--forms-1]').getStyleProperty('-webkit-appearance')).eql('button')
+        .expect(Selector('[type="reset"][data-test--forms-1]').getStyleProperty('-webkit-appearance')).eql('button')
+        .expect(Selector('[type="submit"][data-test--forms-1]').getStyleProperty('-webkit-appearance')).eql('button');
+});
+
+test('Remove the inner border and padding in Firefox.', async t => {
+    // -moz-focus-inner does not exist in chrome
+});
+
+test('Restore the focus styles unset by the previous rule.', async t => {
+    // -moz-focusring does not exist in chrome
+});
+
+test('Correct the padding in Firefox.', async t => {
+    await t
+        .expect(Selector('fieldset[data-test--forms-2]').getStyleProperty('padding-top')).eql('5.6px')
+        .expect(Selector('fieldset[data-test--forms-2]').getStyleProperty('padding-right')).eql('12px')
+        .expect(Selector('fieldset[data-test--forms-2]').getStyleProperty('padding-bottom')).eql('10px')
+        .expect(Selector('fieldset[data-test--forms-2]').getStyleProperty('padding-left')).eql('12px');
+});
+
+test('Remove the padding so developers are not caught out when they zero out `fieldset` elements in all browsers.', async t => {
+    await t
+        .expect(Selector('legend[data-test--forms-2]').getStyleProperty('padding-top')).eql('0px')
+        .expect(Selector('legend[data-test--forms-2]').getStyleProperty('padding-right')).eql('0px')
+        .expect(Selector('legend[data-test--forms-2]').getStyleProperty('padding-bottom')).eql('0px')
+        .expect(Selector('legend[data-test--forms-2]').getStyleProperty('padding-left')).eql('0px');
+});
+
+test('Add the correct vertical alignment in Chrome and Firefox.', async t => {
+    await t
+        .expect(Selector('progress[data-test--forms-2]').getStyleProperty('vertical-align')).eql('baseline');
+});
+
+test('Correct the cursor style of increment and decrement buttons in Safari.', async t => {
+    // TODO: Pseudo-elements selector not supported by testcafe (See https://github.com/DevExpress/testcafe/issues/2813).
+});
+
+test('Correct the odd appearance in Chrome and Safari.', async t => {
+    await t
+        .expect(Selector('[type="search"][data-test--forms-1]').getStyleProperty('-webkit-appearance')).eql('textfield');
+});
+
+test('Correct the outline style in Safari.', async t => {
+    await t
+        .expect(Selector('[type="search"][data-test--forms-1]').getStyleProperty('outline-offset')).eql('0px');
+});
+
+test('Remove the inner padding in Chrome and Safari on macOS.', async t => {
+    // TODO: Pseudo-elements selector not supported by testcafe (See https://github.com/DevExpress/testcafe/issues/2813).
+});
+
+test('Correct the inability to style clickable types in iOS and Safari.', async t => {
+    // TODO: Pseudo-elements selector not supported by testcafe (See https://github.com/DevExpress/testcafe/issues/2813).
+});
+
+test('Change font properties to `inherit` in Safari.', async t => {
+    // TODO: Pseudo-elements selector not supported by testcafe (See https://github.com/DevExpress/testcafe/issues/2813).
+});
+
+test('Add the correct display in Chrome and Safari.', async t => {
+    await t
+        .expect(Selector('summary[data-test--interactive]').getStyleProperty('display')).eql('list-item');
+});
