f37ff5af556b9ef4c24453e918b4fedcce157b17
Sindre Sorhus <sindresorhus@gmail.com>
2019-12-23 12:58:08 +0100

    Replace `BlinkMacSystemFont` font with `system-ui`
    See https://github.com/primer/css/issues/838
    
    Fixes #18

--- test/acceptance/chrome/rules.ts
+++ test/acceptance/chrome/rules.ts
@@ -1,8 +1,5 @@
 import {Selector} from 'testcafe';
 
-// `BlinkMacSystemFont` is being replaced by `system-ui` on macOS
-const SystemUIFont = process.platform === 'darwin' ? 'system-ui' : 'BlinkMacSystemFont';
-
 fixture `Chrome Rules Tests`
 	.page `http://localhost:8080/test/page/with-css.html`;
 
@@ -37,7 +34,7 @@ test('Remove the margin in all browsers.', async t => {
 
 test('Improve consistency of default fonts in all browsers.', async t => {
 	await t
-		.expect(Selector('body').getStyleProperty('font-family')).eql(`-apple-system, ${SystemUIFont}, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`);
+		.expect(Selector('body').getStyleProperty('font-family')).eql(`system-ui, -apple-system, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`);
 });
 
 test('Add the correct height in Firefox.', async t => {
